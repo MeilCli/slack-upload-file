13e8f972d4c722ef4a856e32083b1fd70794a580
MeilCli <ics.sora.meil@gmail.com>
2022-01-09 18:48:14 +0900

    dependency: update

--- src/main.ts
+++ src/main.ts
@@ -66,7 +66,9 @@ async function run() {
         }
         core.setOutput("response", JSON.stringify(result));
     } catch (error) {
-        core.setFailed(error.message);
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
     }
 }
 
