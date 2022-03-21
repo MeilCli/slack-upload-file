58c112b59dc94deab0bd9cb36fcc85f12bf3cdcd
MeilCli <MeilCli@users.noreply.github.com>
2022-03-21 04:18:34 +0000

    update actions

--- dist/index.js
+++ dist/index.js
@@ -29,59 +29,23 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (_) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-var core = __importStar(__nccwpck_require__(2186));
-var slack = __importStar(__nccwpck_require__(431));
-var fs = __importStar(__nccwpck_require__(7147));
-var defaultMaxRetryCount = 3;
+const core = __importStar(__nccwpck_require__(2186));
+const slack = __importStar(__nccwpck_require__(431));
+const fs = __importStar(__nccwpck_require__(7147));
+const defaultMaxRetryCount = 3;
 function getInput(key) {
     return core.getInput(key, { required: true });
 }
 function getInputOrUndefined(key) {
-    var result = core.getInput(key, { required: false });
+    const result = core.getInput(key, { required: false });
     if (result.length == 0) {
         return undefined;
     }
     return result;
 }
 function getInputNumberOrUndefined(key) {
-    var value = getInputOrUndefined(key);
+    const value = getInputOrUndefined(key);
     if (value == undefined) {
         return undefined;
     }
@@ -102,51 +66,38 @@ function readOption() {
         retries: getInputNumberOrUndefined("retries"),
     };
 }
-function run() {
-    var _a, _b;
-    return __awaiter(this, void 0, void 0, function () {
-        var option, client, file, result, error_1;
-        return __generator(this, function (_c) {
-            switch (_c.label) {
-                case 0:
-                    _c.trys.push([0, 2, , 3]);
-                    option = readOption();
-                    client = new slack.WebClient(option.slackToken, {
-                        slackApiUrl: option.slackApiUrl,
-                        retryConfig: { retries: (_a = option.retries) !== null && _a !== void 0 ? _a : defaultMaxRetryCount },
-                    });
-                    file = void 0;
-                    if (option.filePath) {
-                        file = fs.readFileSync(option.filePath);
-                    }
-                    return [4 /*yield*/, client.files.upload({
-                            channels: option.channels,
-                            content: option.content,
-                            file: file,
-                            filename: option.fileName,
-                            filetype: option.fileType,
-                            initial_comment: option.initialComment,
-                            thread_ts: option.threadTs,
-                            title: option.title,
-                        })];
-                case 1:
-                    result = _c.sent();
-                    if (result.ok == false) {
-                        core.setFailed((_b = result.error) !== null && _b !== void 0 ? _b : "unknown error");
-                        return [2 /*return*/];
-                    }
-                    core.setOutput("response", JSON.stringify(result));
-                    return [3 /*break*/, 3];
-                case 2:
-                    error_1 = _c.sent();
-                    if (error_1 instanceof Error) {
-                        core.setFailed(error_1.message);
-                    }
-                    return [3 /*break*/, 3];
-                case 3: return [2 /*return*/];
-            }
+async function run() {
+    try {
+        const option = readOption();
+        const client = new slack.WebClient(option.slackToken, {
+            slackApiUrl: option.slackApiUrl,
+            retryConfig: { retries: option.retries ?? defaultMaxRetryCount },
         });
-    });
+        let file;
+        if (option.filePath) {
+            file = fs.readFileSync(option.filePath);
+        }
+        const result = await client.files.upload({
+            channels: option.channels,
+            content: option.content,
+            file: file,
+            filename: option.fileName,
+            filetype: option.fileType,
+            initial_comment: option.initialComment,
+            thread_ts: option.threadTs,
+            title: option.title,
+        });
+        if (result.ok == false) {
+            core.setFailed(result.error ?? "unknown error");
+            return;
+        }
+        core.setOutput("response", JSON.stringify(result));
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        }
+    }
 }
 run();
 
